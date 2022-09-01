3569924bfc8c20d5510b93589ce7d4b46ab585d8
Andrew Stanton-Nurse <git@analogrelay.net>
2022-08-31 18:42:20 -0700

    update script

--- ReproApp/Program.cs
+++ ReproApp/Program.cs
@@ -1,30 +1,39 @@
-﻿using Microsoft.AspNetCore.DataProtection;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var keysDirectory = Path.Combine(Directory.GetCurrentDirectory(), ".keys");
-Directory.CreateDirectory(keysDirectory);
-var builder = new HostBuilder();
-builder.ConfigureServices(services =>
+public static class Program
 {
-    services.AddDataProtection()
-        .PersistKeysToFileSystem(new DirectoryInfo(keysDirectory));
-});
+    public static async Task Main(string[] args)
+    {
+        var keysDirectory = Path.Combine(Directory.GetCurrentDirectory(), ".keys");
+        Directory.CreateDirectory(keysDirectory);
+        var builder = new HostBuilder();
+        builder.ConfigureServices(services =>
+        {
+            services.AddDataProtection()
+                .PersistKeysToFileSystem(new DirectoryInfo(keysDirectory));
+        });
 
-using var host = builder.Build();
+        using var host = builder.Build();
 
-await host.StartAsync();
+        await host.StartAsync();
 
-var protectionProvider = host.Services.GetRequiredService<IDataProtectionProvider>();
-var protector = protectionProvider.CreateProtector("nonsense");
+        var protectionProvider = host.Services.GetRequiredService<IDataProtectionProvider>();
+        var protector = protectionProvider.CreateProtector("nonsense");
 
-if(args[0] == "protect")
-{
-    var protectedPayload = protector.Protect(args[1]);
-    Console.WriteLine(protectedPayload);
-}
-else if(args[0] == "unprotect")
-{
-    var unprotectedPayload = protector.Unprotect(args[1]);
-    Console.WriteLine(unprotectedPayload);
+        if (args[0] == "protect")
+        {
+            var protectedPayload = protector.Protect(args[1]);
+            Console.WriteLine(protectedPayload);
+        }
+        else if (args[0] == "unprotect")
+        {
+            var unprotectedPayload = protector.Unprotect(args[1]);
+            Console.WriteLine(unprotectedPayload);
+        }
+    }
 }
